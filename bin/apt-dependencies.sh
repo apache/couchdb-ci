46bb45f2f11139b86e51cc36785ff98687fa965e
Will Young <lost.networking@gmail.com>
2022-04-25 21:34:34 +0200

    Drop support for Debian 9 (stretch)

--- bin/apt-dependencies.sh
+++ bin/apt-dependencies.sh
@@ -42,7 +42,7 @@ apt-get update && apt-get install --no-install-recommends -y lsb-release
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${SCRIPTPATH}/detect-arch.sh >/dev/null
 . ${SCRIPTPATH}/detect-os.sh >/dev/null
-debians='(jessie|stretch|buster)'
+debians='(jessie|buster)'
 ubuntus='(bionic|focal)'
 echo "Detected Ubuntu/Debian version: ${VERSION_CODENAME}   arch: ${ARCH}"
 
