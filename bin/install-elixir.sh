715448554bdb246487084c7e884743870f598e75
Nick Vatamaniuc <vatamane@apache.org>
2022-07-02 01:13:29 -0400

    Update defaults for erlang, elixir and node
    Use more recent defaults:
     Erlang - latest 23 version
     Elixir - latest 1.13 version (this one works with Erlang 25)
     Node - update the default one from 10 to 14 (we ship 14 currently)

--- bin/install-elixir.sh
+++ bin/install-elixir.sh
@@ -28,7 +28,7 @@
 set -e
 
 ELIXIR_PACKAGES=https://github.com/elixir-lang/elixir/releases/download
-ELIXIR_VSN=${ELIXIRVERSION:-v1.7.4}
+ELIXIR_VSN=${ELIXIRVERSION:-v1.13.4}
 
 # Check if running as root
 if [[ ${EUID} -ne 0 ]]; then
